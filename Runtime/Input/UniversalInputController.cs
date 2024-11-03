d36febfabf19d356efb00aa572f3b10687c77db3
~skysfall1ng~ <skythecreative@gmail.com>
2024-11-02 21:11:13 -0700

    Update UniversalInputController.cs

--- Runtime/Input/UniversalInputController.cs
+++ Runtime/Input/UniversalInputController.cs
@@ -4,63 +4,64 @@ using UnityEngine;
 
 namespace Darklight.UnityExt.Input
 {
-    public abstract class UniversalInputController : MonoBehaviour
-    {
-        [Header("Input")]
-        [SerializeField, ShowOnly] Vector2 _moveInput;
-        [SerializeField, ShowOnly] bool _moveInputStarted;
-        [SerializeField, ShowOnly] bool _moveInputCanceled;
+	public abstract class UniversalInputController : MonoBehaviour
+	{
+		[Header("Input")]
+		[SerializeField, ShowOnly] Vector2 _moveInput;
+		[SerializeField, ShowOnly] bool _moveInputStarted;
+		[SerializeField, ShowOnly] bool _moveInputCanceled;
 
-        public Vector2 MoveInput => _moveInput;
+		public Vector2 MoveInput => _moveInput;
+		public bool IsMoving => MoveInput.magnitude > 0;
 
-        protected virtual void Awake()
-        {
-            EnableInputs();
-        }
+		protected virtual void Awake()
+		{
+			EnableInputs();
+		}
 
-        #region < NONPUBLIC_METHODS > [[ HANDLE INPUTS ]] ================================================================ 
-        protected virtual void EnableInputs()
-        {
-            UniversalInputManager.OnMoveInputStarted += HandleOnMoveInputStarted;
-            UniversalInputManager.OnMoveInput += HandleOnMoveInput;
-            UniversalInputManager.OnMoveInputCanceled += HandleOnMoveInputCanceled;
-        }
+		#region < NONPUBLIC_METHODS > [[ HANDLE INPUTS ]] ================================================================ 
+		protected virtual void EnableInputs()
+		{
+			UniversalInputManager.OnMoveInputStarted += HandleOnMoveInputStarted;
+			UniversalInputManager.OnMoveInput += HandleOnMoveInput;
+			UniversalInputManager.OnMoveInputCanceled += HandleOnMoveInputCanceled;
+		}
 
-        protected virtual void DisableInputs()
-        {
-            UniversalInputManager.OnMoveInputStarted -= HandleOnMoveInputStarted;
-            UniversalInputManager.OnMoveInput -= HandleOnMoveInput;
-            UniversalInputManager.OnMoveInputCanceled -= HandleOnMoveInputCanceled;
-        }
+		protected virtual void DisableInputs()
+		{
+			UniversalInputManager.OnMoveInputStarted -= HandleOnMoveInputStarted;
+			UniversalInputManager.OnMoveInput -= HandleOnMoveInput;
+			UniversalInputManager.OnMoveInputCanceled -= HandleOnMoveInputCanceled;
+		}
 
-        protected virtual void HandleOnMoveInputStarted(Vector2 moveInput)
-        {
-            _moveInput = moveInput;
-            _moveInputStarted = true;
-        }
+		protected virtual void HandleOnMoveInputStarted(Vector2 moveInput)
+		{
+			_moveInput = moveInput;
+			_moveInputStarted = true;
+		}
 
-        protected virtual void HandleOnMoveInput(Vector2 moveInput)
-        {
-            _moveInput = moveInput;
-        }
+		protected virtual void HandleOnMoveInput(Vector2 moveInput)
+		{
+			_moveInput = moveInput;
+		}
 
-        protected virtual void HandleOnMoveInputCanceled()
-        {
-            _moveInput = Vector2.zero;
-            _moveInputStarted = false;
-            _moveInputCanceled = true;
-            StartCoroutine(MoveInputCanceled());
-        }
+		protected virtual void HandleOnMoveInputCanceled()
+		{
+			_moveInput = Vector2.zero;
+			_moveInputStarted = false;
+			_moveInputCanceled = true;
+			StartCoroutine(MoveInputCanceled());
+		}
 
-        IEnumerator MoveInputCanceled()
-        {
-            yield return new WaitForEndOfFrame();
-            _moveInputCanceled = true;
+		IEnumerator MoveInputCanceled()
+		{
+			yield return new WaitForEndOfFrame();
+			_moveInputCanceled = true;
 
-            yield return new WaitForEndOfFrame();
-            _moveInputCanceled = false;
-        }
+			yield return new WaitForEndOfFrame();
+			_moveInputCanceled = false;
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
