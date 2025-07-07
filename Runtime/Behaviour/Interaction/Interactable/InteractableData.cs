4be8647a8776d6314d28ea1b6c7f64233bd0202e
~skysfall1ng~ <skythecreative@gmail.com>
2025-07-07 15:06:36 -0700

    move id to interactable class

--- Runtime/Behaviour/Interaction/Interactable/InteractableData.cs
+++ Runtime/Behaviour/Interaction/Interactable/InteractableData.cs
@@ -6,19 +6,10 @@ namespace Darklight.Behaviour
     public abstract class InteractableData : ScriptableObject
     {
         [SerializeField]
-        private int _id;
-
-        [SerializeField]
         private string _key;
 
         [SerializeField]
         private LayerMask _layer;
-
-        public int ID
-        {
-            get => _id;
-            protected set => _id = value;
-        }
 
         public string Key
         {
@@ -32,25 +23,15 @@ namespace Darklight.Behaviour
             protected set => _layer = value;
         }
 
-        public virtual void GenerateData(Interactable interactable)
+        public virtual void SetData(string key, LayerMask layer)
         {
-            SetData(
-                interactable.gameObject.GetInstanceID(),
-                interactable.gameObject.name,
-                interactable.gameObject.layer
-            );
-        }
-
-        public virtual void SetData(int id, string key, LayerMask layer)
-        {
-            _id = id;
             _key = key;
             _layer = layer;
         }
 
         public virtual void CopyData(InteractableData data)
         {
-            SetData(data.ID, data.Key, data.Layer);
+            SetData(data.Key, data.Layer);
         }
     }
 }
